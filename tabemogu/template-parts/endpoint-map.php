81ee59e920bdba9f4db124b280f7936e7c67964f
Hitoshi Omagari <hitoshi@prime-strategy.co.jp>
2016-08-25 12:11:20 +0900

    coupon bug fix
    クーポンの条件分岐の位置を間違えてた。。

--- tabemogu/template-parts/endpoint-map.php
+++ tabemogu/template-parts/endpoint-map.php
@@ -8,9 +8,9 @@ if ( $map ) :
 	<?php echo wp_get_attachment_image( $map, 'full' ); ?>
 <?php
 endif;
+if ( $coupon ) :
 ?>
 	<h2>クーポン</h2>
 	<?php echo wp_get_attachment_image( $coupon, 'full' ); ?>
 <?php
-if ( $coupon ) :
 endif;
